fc1a91496bf90d395d8bac5cfb7f0d8aa2c7b027
The-Gentleman <zraja4402@gmail.com>
2020-12-18 21:38:48 -0500

    added create route + action

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::PatientsController < ApplicationController
         patients = Patient.all 
         render json: patients
     end 
+
+    def create
+        patient = Patient.new(patient_params)
+        if patient.save 
+            render json: patient, status: :accepted
+        else 
+            render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+
+    private
+    def patient_params
+        params.require(:patient).permit(:name, :diagnosis)
+    end 
 end
