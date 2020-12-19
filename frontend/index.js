a85657d5ebc0244dfe6697285b80eb2aff3be650
The-Gentleman <zraja4402@gmail.com>
2020-12-19 00:50:20 -0500

    refactored fetch into seperate function

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,16 @@
 // WHEN *SOME EVENT HAPPENS*, I WANT TO MAKE *WHAT KIND OF FETCH*, AND THEN *MAINPULATE THE DOM* IN WHAT WAY?
 
 document.addEventListener('DOMContentLoaded', () => {
+    getPatients();
+})
+
+function getPatients(){
     const patientEndPoint = 'http://localhost:3000/api/v1/patients'
+    // 1 of 3
     fetch(patientEndPoint)
         .then(response => response.json())
         .then(patients => {
             console.log(patients)
         })
-})
+
+}
