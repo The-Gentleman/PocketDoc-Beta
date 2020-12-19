0758ce47b518a3548f98eb1e0b62e4d5238cd952
The-Gentleman <zraja4402@gmail.com>
2020-12-18 23:09:20 -0500

    added create action

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -5,4 +5,16 @@ class Api::V1::ExercisesController < ApplicationController
         render json: ExerciseSerializer.new(exercises)
     end 
 
+    def create
+        exercsise = Exercise.new(exercise_params)
+        if exercsise.save 
+            render json: exercsise, status: :accepted
+        else 
+            render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity 
+            # unprocessible_entity is a 402 error saying that syntax is okay, 
+            # but you probably failed some validations
+        end 
+
+    end 
+
 end
